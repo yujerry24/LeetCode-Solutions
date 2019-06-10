5f04314ea4a61242f9a0892ad4e19f5f0ad72c08
Jerry Yu <yujerry24@gmail.com>
2019-06-10 14:10:32 -0400

    Add files via upload

---
+++ Easy/ValidBoomerang.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isBoomerang(int[][] points) {                                                                            if(points[0][0]==points[1][0] && points[0][1]==points[1][1]) {
+            return false;
+        }
+        if(points[0][0]==points[2][0] && points[0][1]==points[2][1]) {
+            return false;
+        }
+        if(points[1][0]==points[2][0] && points[1][1]==points[2][1]) {
+            return false;
+        }
+        double slope1=(points[1][1]-points[0][1])/1.0/(points[1][0]-points[0][0]);
+        double slope2=(points[2][1]-points[1][1])/1.0/(points[2][0]-points[1][0]);
+        if(slope1==slope2) {
+            return false;
+        }
+        return true;
+                                                
+        
+    }
+}
