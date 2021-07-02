746ee4bac2585eacaddccd64f891b3b3bd715ace
Jerry Yu <yujerry24@gmail.com>
2021-07-02 00:15:41 -0400

    Update GroupAnagrams.cpp

--- Array/GroupAnagrams.cpp
+++ Array/GroupAnagrams.cpp
@@ -1,21 +1,27 @@
+#include <iostream>
+#include <utility>
+#include <functional>
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string, vector<string>> set;
+        unordered_map<string, vector<string>> set;
         vector<vector<string>> answer;
         for (int i = 0; i< strs.size(); i++) {
             string temp = strs.at(i);
             sort(temp.begin(), temp.end());
             if (set.find(temp) == set.end()) {
+                cout<<"hello"<<endl;
                 vector<string> cur;
                 cur.push_back(strs.at(i));
-                set.emplace(make_pair<string, vector<string>>(temp,  cur);
+                set.emplace(temp,  cur);
             } else {
-
+                vector<string> cur = set.find(temp)->second;
+                cur.push_back(strs.at(i));
+                set.at(temp) =   cur;
             }
         }
         for (auto const& x : set) {
-            answer.push_back(x.second());
+            answer.push_back(x.second);
         }
         return answer;
     }
