c90952c4fc4d6738b83b93151260535b382b92ab
Jerry Yu <yujerry24@gmail.com>
2021-07-01 23:58:40 -0400

    Create GroupAnagrams.cpp

---
+++ Array/GroupAnagrams.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string, vector<string>> set;
+        vector<vector<string>> answer;
+        for (int i = 0; i< strs.size(); i++) {
+            string temp = strs.at(i);
+            sort(temp.begin(), temp.end());
+            if (set.find(temp) == set.end()) {
+                set.emplace(make_pair<string, vector<string>>(temp,  set.find(temp)->second));
+            }
+        }
+        for (auto const& x : set) {
+            answer.push_back(x.second());
+        }
+        return answer;
+    }
+};
