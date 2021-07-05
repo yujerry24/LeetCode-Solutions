3fe1407db96de6b1f4c0a150b7a9aa84f36f8845
Jerry Yu <yujerry24@gmail.com>
2021-07-04 23:59:11 -0400

    linted

--- Array/JumpGameII.cpp
+++ Array/JumpGameII.cpp
@@ -1,31 +1,29 @@
-
-
 class Solution {
-public:
-    int jump(vector<int>& nums) {
-        int currentPosition = 0;
-        int nextPosition = 0;
-        int farthestPosition = 0;
-        int jumps = 0;
-        while (currentPosition < nums.size() -1 ) {
-            for (int i = currentPosition+1; i <= currentPosition + nums.at(currentPosition); i++) {
-                if (i >= nums.size()) {
-                    continue;
-                } else {
-                    if (i == nums.size()-1) {
-                        nextPosition = nums.size()-1;
-                    }
-                if (i + nums[i]> farthestPosition) {
-                    farthestPosition = i + nums[i];
-                    nextPosition = i;
-                }
-            
-                }
+  public:
+    int jump(vector < int > & nums) {
+      int currentPosition = 0;
+      int nextPosition = 0;
+      int farthestPosition = 0;
+      int jumps = 0;
+      while (currentPosition < nums.size() - 1) {
+        for (int i = currentPosition + 1; i <= currentPosition + nums.at(currentPosition); i++) {
+          if (i >= nums.size()) {
+            continue;
+          } else {
+            if (i == nums.size() - 1) {
+              nextPosition = nums.size() - 1;
             }
-            currentPosition = nextPosition;
-            farthestPosition = 0;
-            jumps++;
+            if (i + nums[i] > farthestPosition) {
+              farthestPosition = i + nums[i];
+              nextPosition = i;
+            }
+
+          }
         }
-        return jumps;
+        currentPosition = nextPosition;
+        farthestPosition = 0;
+        jumps++;
+      }
+      return jumps;
     }
 };
