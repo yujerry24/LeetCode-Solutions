d7d5bb2c93c225c8f8445052ffcdafb9734842a2
Jerry Yu <yujerry24@gmail.com>
2021-07-11 11:45:54 -0400

    Update maximumProfitJobScheduling.ts

--- Array/maximumProfitJobScheduling.ts
+++ Array/maximumProfitJobScheduling.ts
@@ -5,7 +5,29 @@ function sortFunction(a: number[], b: number[]) {
     return a[0] < b[0] ? -1 : 1;
   }
 }
-function recurse(index: number, jobs: number[][], dpArray: number[]) {}
+
+function findNext(index: number, jobs: number[][]): number {
+  for (let i = index + 1; i < jobs.length; i++) {
+    if (jobs[i][0] >= jobs[index][1]) {
+      return i;
+    }
+  }
+  return -1;
+}
+function recurse(index: number, jobs: number[][], dpArray: number[]) {
+  if (index == jobs.length) {
+    return 0;
+  }
+  if (dpArray[index] !== undefined) {
+    return dpArray[index];
+  }
+  let nextJob = findNext(index, jobs);
+  let takeProfit =
+    jobs[index][2] + (nextJob === -1 ? 0 : recurse(nextJob, jobs, dpArray));
+  let remainProfit = recurse(index + 1, jobs, dpArray);
+  dpArray[index] = Math.max(takeProfit, remainProfit);
+  return dpArray[index];
+}
 function jobScheduling(
   startTime: number[],
   endTime: number[],
@@ -18,6 +40,5 @@ function jobScheduling(
   jobs.sort(sortFunction);
   console.log(jobs);
   let dp: number[] = new Array(startTime.length);
-  recurse(0, jobs, dp);
-  return dp[startTime.length];
+  return recurse(0, jobs, dp);
 }
